fc056e5d2a1f96eb36a94fdc275d3fab4e42b082
Nik Hixson <nikilobe@gmail.com>
2024-12-10 17:40:11 -0800

    Add more SQL queries for teachers and students and cohorts

--- 3_queries_group-by/low_average_completion_time.sql
+++ 3_queries_group-by/low_average_completion_time.sql
@@ -1,14 +1,8 @@
-WITH avg_estimated_time AS (
-    SELECT AVG(duration) AS avg_duration
-    FROM assignments
-)
-SELECT students.name AS student, 
-       AVG(assignment_submissions.duration) AS average_assignment_duration,
-       avg_estimated_time.avg_duration AS average_estimated_duration
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
 FROM students
-JOIN assignment_submissions ON students.id = assignment_submissions.student_id
-JOIN avg_estimated_time ON true
-WHERE students.end_date IS NULL
-GROUP BY students.name, avg_estimated_time.avg_duration
-HAVING AVG(assignment_submissions.duration) < avg_estimated_time.avg_duration
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
 ORDER BY average_assignment_duration;
---
+++ 4_queries/total_assignments_and_duration.sql
@@ -0,0 +1,6 @@
+SELECT day,
+       COUNT(*) AS number_of_assignments,
+       SUM(duration) AS total_duration
+FROM assignments
+GROUP BY day
+ORDER BY day;
